5cf424a7dca4211f482d4ece8281fb25ccf6e429
Jared Sewak <jaredsewak@gmail.com>
2023-07-04 17:58:25 -0600

    boolean to a string

---
+++ b_2_s.py
@@ -0,0 +1,13 @@
+"""
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+"""
+
+def boolean_to_string(b):
+    return str(b)
+
+my_boolean = True
+string_value = boolean_to_string(my_boolean)
+print(string_value)
