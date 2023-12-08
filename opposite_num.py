1002a3c6637adb6c467b13199f0f98ae0399f196
Jared Sewak <jaredsewak@gmail.com>
2023-12-08 13:51:30 -0700

    find opposite num

---
+++ opposite_num.py
@@ -0,0 +1,18 @@
+"""
+Very simple, 
+given an integer or a floating-point number, 
+find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+"""
+
+def opposite(number):
+  return -1 * number
+
+print(opposite(1))
+print(opposite(14))
+print(opposite(-34))
